5fa420b89e48e59212b64612662e61704547f791
ariumaschi <aumaschi@udesa.edu.ar>
2021-11-14 13:15:40 -0300

    genre.js terminado y algunas modificaciones de css

---
+++ js/genres.js
@@ -0,0 +1,85 @@
+let url = `https://api.themoviedb.org/3/genre/movie/list?api_key=e88616470bd2ffe2b246bcbf04162b02`;
+
+    console.log(url)
+
+fetch(url)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        let info = data.genres
+        let generosPelis = ""
+        let capturo = document.querySelector("div.div-generos-p")
+
+        for (let i = 0; i < info.length; i++) {
+            generosPelis +=
+                `<article class="article-generos">
+                    <a href="detail-genres.html?id_G_Movie=${info[i].id}&name_G_Movie=${info[i].name}"> ${info[i].name} </a>
+                </article>`
+                
+                
+ }
+
+        capturo.innerHTML = generosPelis;
+        
+        
+
+        
+
+    })
+
+    
+
+
+
+    .catch(function (error) {
+        console.log("Error: " + error)
+    })
+
+    //SERIES//
+
+    let url2 = `https://api.themoviedb.org/3/genre/tv/list?api_key=e88616470bd2ffe2b246bcbf04162b02`;
+
+console.log(url2)
+
+fetch(url2)
+    .then(function (response) {
+        return response.json();
+    })
+    
+    .then(function (data) {
+        console.log(data);
+        let info = data.genres
+        let generosSeries = ""
+        let capturo = document.querySelector("div.div-generos-s")
+
+        for (let i = 0; i < info.length; i++) {
+            generosSeries +=
+                `<article class="article-generos">
+                    <a href="detail-genres.html?id_G_Series=${info[i].id}&name_G_Series=${info[i].name}"> ${info[i].name} </a>
+                </article>`
+                
+
+                
+ }
+
+        capturo.innerHTML = generosSeries;
+        
+      
+                  
+
+    })
+
+    
+
+
+
+    .catch(function (error) {
+        console.log("Error: " + error)
+    })
+
+
+
+
+
