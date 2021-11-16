6e71aad958474bce7d30f2b469103c01072d09a2
Carol Ejdin <carolejdin@MacBook-Air-de-Carol.local>
2021-11-16 10:57:08 -0300

    agregue resultados pelis

--- js/resultadobusquedapeli.js
+++ js/resultadobusquedapeli.js
@@ -1,14 +1,14 @@
 //buscador series
-let queryString = location.search // capturando la query que se envio cuando el usuario busco una palabra
-console.log(queryString)
-let queryString2 = new URLSearchParams(queryString);  // pasala a un objeto literal
-let query = queryString2.get('buscador'); // capturando el input que puso el usuario (LA PROPIEDAD BUSCADOR DE LA QUERY)
-console.log(queryString);
-console.log(query)
+let queryStringg = location.search // capturando la query que se envio cuando el usuario busco una palabra
+console.log(queryStringg)
+let queryString3 = new URLSearchParams(queryStringg);  // pasala a un objeto literal
+let query1 = queryString3.get('buscador'); // capturando el input que puso el usuario (LA PROPIEDAD BUSCADOR DE LA QUERY)
+console.log(queryStringg);
+console.log(query1)
 // queremos a partir de la palabra que capturamos traer todo lo que tiene la api en relacion a eso
 
-let url = `https://api.themoviedb.org/3/search/movie?api_key=63cdfcbb1edb0e2c2331f8b2cb24ba9b&query=${query}`;
-fetch(url)
+let url1= `https://api.themoviedb.org/3/search/tv?api_key=63cdfcbb1edb0e2c2331f8b2cb24ba9b&query=${query1}`;
+fetch(url1)
     .then(function (response) {
         return response.json()
     })
@@ -30,10 +30,10 @@ fetch(url)
 
     <div class="padre-info-resultados">
         <a href="detailmovie.html">
-            <h2 class="Titulo-de-peli-resultados">${info[i].name}</h2>
+            <h2 class="Titulo-de-peli-resultados">${info[i].title}</h2>
         </a>
 
-        <h3 class="Fecha-estreno-resultados">${info[i].first_air_date}</h3>
+        <h3 class="Fecha-estreno-resultados">${info[i].release_date}</h3>
 
         <p class="sinopsis-resultados">${info[i].overview}</p>
     </div>`
